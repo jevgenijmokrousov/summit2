d278fc2273f5b3459d24f90c9ae49d4d79d0d4a3
Jevgenij Mokrousov <info@74dpi.com>
2016-05-25 17:22:08 +0100

    Css: Lesshat mixins

--- Gruntfile.summit.js
+++ Gruntfile.summit.js
@@ -6,14 +6,4 @@ var defaultGruntfile    = require('./Gruntfile'),
 var summitGruntfile = {};
     summitGruntfile.themes = require('./dev/tools/grunt/configs/themes.summit');
 
-    //summitGruntfile.refresh  = function(){
-    //    var tasks = [];
-    //    _.each(summitGruntfile.themes, function(theme, name) {
-    //        tasks.push('clean:' + name);
-    //        tasks.push('exec:' + name);
-    //        tasks.push('less:' + name);
-    //    });
-    //    grunt.task.run(tasks);
-    //}
-
 module.exports = _.extend(defaultGruntfile, summitGruntfile);
